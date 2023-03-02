37c3e1f2f42e75ec5ef919481ecc56deb6edc8e2
Pkow <pkow1999@gmail.com>
2023-03-02 17:04:55 +0100

    Added diceroller ability by bolding pools

--- diceroller.cpp
+++ diceroller.cpp
@@ -1,5 +1,9 @@
 #include "diceroller.h"
 #include "ui_diceroller.h"
+#include "mainwindow.h"
+
+#include <QCheckBox>
+#include <QRandomGenerator>
 
 DiceRoller::DiceRoller(QWidget *parent) :
     QWidget(parent),
@@ -9,15 +13,107 @@ DiceRoller::DiceRoller(QWidget *parent) :
 
 }
 
-DiceRoller::DiceRoller(QWidget *parent, const QStringList &listOfAttributes) :
+DiceRoller::DiceRoller(QWidget *parent, const QStringList &listOfAttributes, const QStringList &listOfSkills) :
     QWidget(parent),
     ui(new Ui::DiceRoller)
 {
     ui->setupUi(this);
-    ui->comboBox->addItems(listOfAttributes);
+    ui->attributeComboBox->addItems(listOfAttributes);
+    ui->skillComboBox->addItems(listOfSkills);
 }
 
 DiceRoller::~DiceRoller()
 {
     delete ui;
 }
+
+void DiceRoller::on_pushButton_clicked()//updateInfo
+{
+    QString Text = "";
+    QMapIterator<QString, int> i(MainWindow::mapOfSkillsWithValue);
+    while (i.hasNext()) {
+        i.next();
+        Text += i.key() + ": " + QString::number(i.value()) + ", ";
+    }
+    ui->textBrowser->setText(Text);
+}
+
+
+void DiceRoller::deleteDices(int size_)
+{
+    for(int i = 0; i < size_; i++)
+    {
+        for(int j = 0; j < 2; j++)
+        {
+            ui->Rolls->itemAt(i)->layout()->itemAt(j)->widget()->deleteLater();
+        }
+        ui->Rolls->itemAt(i)->layout()->deleteLater();
+    }
+}
+
+void DiceRoller::on_rollButton_clicked()
+{
+    deleteDices(MainWindow::counter);
+    MainWindow::counter = 0;
+    //MainWindow::hunger = countDots(ui->Hunger);
+
+    QMapIterator<QString, int> i(MainWindow::mapOfSkillsWithValue);
+    while (i.hasNext()) {
+        i.next();
+        MainWindow::counter += i.value();
+    }
+    MainWindow::counter += ui->diceModifier->value();
+    createDices(MainWindow::counter);
+}
+
+void DiceRoller::createDices(int size_)
+{
+    for(int i = 0; i < size_; i++)
+    {
+        QCheckBox *dynCheck = new QCheckBox(this);
+        dynCheck->setStyleSheet("QCheckBox::indicator{width: 15px;height: 15px;}");
+        dynCheck->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
+        QVBoxLayout *dynLayout = new QVBoxLayout();
+        dynLayout->setSizeConstraint(QLayout::SetFixedSize);
+        int generatedNumber = QRandomGenerator::system()->bounded(10) + 1;
+        QLabel *dynLabel = new QLabel(this);
+        dynLabel->setAlignment(Qt::AlignCenter);
+        dynLabel->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
+
+        dynLabel->setText(QString::number(generatedNumber));
+        dynLabel->setStyleSheet("QLabel { font-size : 20px;}");
+        dynCheck->setObjectName("czek" + QString::number(i));
+        dynCheck->setCheckable(true);
+        if(i > MainWindow::counter - MainWindow::hunger - 1)
+        {
+            dynLabel->setStyleSheet("QLabel { color : red; font-size : 20px;}");
+        }
+        dynLayout->addWidget(dynLabel);
+        dynLayout->addWidget(dynCheck);
+        dynLayout->setAlignment(dynLabel, Qt::AlignCenter);
+        dynLayout->setAlignment(dynCheck, Qt::AlignCenter);
+        ui->Rolls->addLayout(dynLayout);
+    }
+}
+
+
+void DiceRoller::on_rerollButton_clicked()
+{
+    for(int i = 0; i < MainWindow::counter; i++)
+    {
+        QAbstractButton *bt = qobject_cast<QAbstractButton *>(ui->Rolls->itemAt(i)->layout()->itemAt(1)->widget());
+        if(bt->isChecked())
+        {
+            int generatedNumber = QRandomGenerator::system()->bounded(10) + 1;
+            {
+                QLabel *lb = qobject_cast<QLabel *>(ui->Rolls->itemAt(i)->layout()->itemAt(0)->widget());
+                lb->setText(QString::number(generatedNumber));
+                if(lb->styleSheet() == "QLabel { color : red; font-size : 20px;}"){
+                    lb->setStyleSheet("QLabel { color : purple; font-size: 20px;}");
+                }
+                else lb->setStyleSheet(" QLabel{color : blue; font-size : 20px;}");
+            }
+        }
+    }
+}
+
--- diceroller.h
+++ diceroller.h
@@ -15,8 +15,15 @@ class DiceRoller : public QWidget
 
 public:
     DiceRoller(QWidget *parent = nullptr);
-    DiceRoller(QWidget *parent = nullptr, const QStringList &listOfAttributes = QStringList());
+    DiceRoller(QWidget *parent = nullptr, const QStringList &listOfAttributes = QStringList(), const QStringList &listOfSkills = QStringList());
     ~DiceRoller();
+
+private slots:
+    void on_pushButton_clicked();
+
+    void on_rollButton_clicked();
+
+    void on_rerollButton_clicked();
 
 private:
     Ui::DiceRoller *ui;
@@ -27,6 +34,8 @@ private:
         QPainter p(this);
         style()->drawPrimitive(QStyle::PE_Widget, &opt, &p, this);
     }
+    void deleteDices(int size_);
+    void createDices(int size_);
 };
 
 #endif // DICEROLLER_H
